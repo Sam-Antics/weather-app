effb55f4bdb63ec7c7797d9bf3d42908b09f80d2
SEGuerra <sameguerra@sbcglobal.net>
2022-06-12 22:35:54 -0500

    added current weather from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
+/******************* ACCEPTANCE CRITERIA FOR REFERENCE ***************************/
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+/*********************************************************************************/
+
 // Global Variables
 var formEl = document.querySelector("#search-form");
-var pastSearch = document.querySelector("#past-cities");
+var pastSearchEl = document.querySelector("#past-cities");
+var currentContainerEl = document.querySelector("#current-container");
+var futureContainerEl = document.querySelector("#future-weather");
+var citySearchedEl = document.querySelector("#city-searched");
+
 
 // API variables
 var apiKey = "7b6408f8c7e6faa6a358eb8a39178c50"
@@ -28,13 +42,89 @@ var searchFormHandler = function(event) {
   var city = searchInfo.replace(" ", "+");
   
   // query API
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey +"&units=imperial";
   console.log(queryURL);
   
+  // make a request to the url
   fetch(queryURL)
+  .then(function(response) {
+    // as long as it's a valid city ...
+    if (response.ok) {
+      response.json().then(function(data) {
+        // ... send to current weather function
+        displayCurrentWeather(data, city);
+      });
+      // otherwise, return error message
+    } else {
+      alert("Error: City Not Found.");
+    }
+  })
+  // in case internet or API is down, let the user know
+  .catch(function(error) {
+    alert("Unable to connect to OpenWeather API");
+  });
   
   // call 'save to localStorage' function
   saveInfo();
+}
+
+// display the current weather
+var displayCurrentWeather = function(conditions, location) {
+  //clear old content
+  currentContainerEl.textContent = "";
+  location = location.replace("+", " ");
+  // display city name from search criteria
+  citySearchedEl.textContent = location;
+
+  console.log(conditions, location);
+
+  // display the current date
+  var currentDate = document.createElement("span")
+  currentDate.textContent=" (" + moment(conditions.dt.value).format("MMM D, YYYY") + ") ";
+  citySearchedEl.appendChild(currentDate);
+
+  // display the icon for current conditions
+  var currentIcon = document.createElement("img")
+  currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + conditions.weather[0].icon + "@2x.png");
+  currentDate.appendChild(currentIcon);
+  
+  
+  // ... the temperature, the humidity, the wind speed, and the UV index
+  // display span for the temp data
+  var tempEl = document.createElement("span");
+  tempEl.textContent = "Current Temperature: " + conditions.main.temp + " °F";
+  currentContainerEl.appendChild(tempEl);
+
+  // display span for humidity data
+  var humidEl = document.createElement("span");
+  humidEl.textContent = "Humidity: " + conditions.main.humidity + " %";
+  currentContainerEl.appendChild(humidEl);
+
+  // display span for wind speed
+  var windEl = document.createElement("span");
+  windEl.textContent = "Wind Speed: " + conditions.wind.speed + " miles/hour";
+  currentContainerEl.appendChild(windEl);
+
+  // getting UV index
+  var lat = conditions.coord.lat;
+  var lon = conditions.coord.lon;
+  getUvIndex(lat, lon);
+}
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+var getUvIndex = function(lat, lon) {
+  var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+  fetch(uvURL)
+  .then(function(response) {
+    response.json().then(function(data) {
+      displayUv(data)
+    });
+  });
+}
+
+var displayUv = function(uvIndex) {
+  console.log(uvIndex);
 }
 
 // put searched city in localStorage 
