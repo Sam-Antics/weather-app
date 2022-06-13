0736c954ee9e5305155dd870569ac517ba2f98e9
SEGuerra <sameguerra@sbcglobal.net>
2022-06-13 03:14:00 -0500

    got the 5-day forecast cards mostly figured out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,10 @@
-/******************* ACCEPTANCE CRITERIA FOR REFERENCE ***************************/
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-/*********************************************************************************/
-
 // Global Variables
 var formEl = document.querySelector("#search-form");
 var currentContainerEl = document.querySelector("#current-container");
-var futureContainerEl = document.querySelector("#future-weather");
 var citySearchedEl = document.querySelector("#city-searched");
 var pastCityBtnEl = document.querySelector("#past-cities-btns");
+var futureContainerEl = document.querySelector("#forecast-container");
+var forecastHeading = document.querySelector("#weather-forecast");
 
 
 // API variables
@@ -39,11 +30,10 @@ var searchFormHandler = function(event) {
   // CALL API WITH SEARCH CRITERIA
   // remove any spaces from 'searchInfo' and place in 'city' variable
   searchInfo = searchInfo.trim();
-  var city = searchInfo.replace(" ", "+");
+  city = searchInfo.replace(" ", "+");
   
   // query API
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey +"&units=imperial";
-  console.log(queryURL);
   
   // make a request to the url
   fetch(queryURL)
@@ -66,7 +56,7 @@ var searchFormHandler = function(event) {
   
   // call 'save to localStorage' function
   // saveInfo();
-
+  
   pastSearches(city);
 }
 
@@ -77,12 +67,12 @@ var displayCurrentWeather = function(conditions, location) {
   location = location.replace("+", " ");
   // display city name from search criteria
   citySearchedEl.textContent = location;
-
+  
   // display the current date
   var currentDate = document.createElement("span")
   currentDate.textContent=" (" + moment(conditions.dt.value).format("MMM D, YYYY") + ") ";
   citySearchedEl.appendChild(currentDate);
-
+  
   // display the icon for current conditions
   var currentIcon = document.createElement("img")
   currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + conditions.weather[0].icon + "@2x.png");
@@ -94,21 +84,25 @@ var displayCurrentWeather = function(conditions, location) {
   var tempEl = document.createElement("span");
   tempEl.textContent = "Current Temperature: " + conditions.main.temp + " °F";
   currentContainerEl.appendChild(tempEl);
-
+  
   // display span for humidity data
   var humidEl = document.createElement("span");
-  humidEl.textContent = "Humidity: " + conditions.main.humidity + " %";
+  humidEl.textContent = "Humidity: " + conditions.main.humidity + "%";
   currentContainerEl.appendChild(humidEl);
-
+  
   // display span for wind speed
   var windEl = document.createElement("span");
   windEl.textContent = "Wind Speed: " + conditions.wind.speed + " miles/hour";
   currentContainerEl.appendChild(windEl);
 
-  // getting UV index
+  // calling UV index
   var lat = conditions.coord.lat;
   var lon = conditions.coord.lon;
   getUvIndex(lat, lon);
+
+  // calling 5-day forecast
+  city = location.replace(" ", "+");
+  getForecast(city);
 }
 
 // WHEN I view the UV index
@@ -124,15 +118,13 @@ var getUvIndex = function(lat, lon) {
 }
 
 var displayUv = function(uvIndex) {
-  console.log(uvIndex);
-
   var uvIndexEl = document.createElement("div");
   uvIndexEl.textContent = "UV Index: "
   currentContainerEl.appendChild(uvIndexEl);
 
   var indexNum = document.createElement("span");
   indexNum.textContent = uvIndex.value;
-
+  
   // assign CSS classes to the spans to indicate the background colors
   if (uvIndex.value <= 3) {
     indexNum.className = "favorable"
@@ -141,47 +133,100 @@ var displayUv = function(uvIndex) {
   } else {
     indexNum.className = "severe"
   };
-
-uvIndexEl.appendChild(indexNum);
+  
+  uvIndexEl.appendChild(indexNum);
 }
 
-// // put searched city in localStorage to pass to Past Cities area
-// var saveInfo = function() {
-//   localStorage.setItem("city", JSON.stringify(searchInfo));
-// }
+// get the future weather 
+var getForecast = function(city) {
+  var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+  fetch(forecastURL) 
+  .then(function(response) {
+    response.json().then(function(data) {
+      displayForecast(data);
+    });
+  });
+}
 
-// put searched city in Searched Cities area from each search made
+// build cards dynamically to show the 5-day forecast
+var displayForecast = function(forecastConditions) {
+  futureContainerEl.textContent = "";
+  forecastHeading.textContent = "5-Day Forecast:";
+
+  // loop through list 
+  var weather = forecastConditions.list;
+
+    // the list object has an iteration every 3 hours on a 24-hour cycle (8 iterations)
+    // this is how I figured out how to loop through them and get 5 days worth of data
+    for (var i = 5; i < weather.length; i = i+8) {
+      var dailyWeather = weather[i];
+
+      console.log(dailyWeather);
+      // create div element for each card
+      var dailyWeatherEl = document.createElement("div");
+      dailyWeatherEl.classList = "card m-2";
+      futureContainerEl.appendChild(dailyWeatherEl);
+
+      // display date on card
+      var weatherDate = document.createElement("h4");
+      weatherDate.textContent = moment.unix(dailyWeather.dt).format("MMM D, YYYY");
+      weatherDate.classList = "text-center card-header";
+      dailyWeatherEl.appendChild(weatherDate);
+
+      // display icon on card
+      var weatherIcon = document.createElement("img");
+      weatherIcon.classList = "text-center card-item";
+      weatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + dailyWeather.weather[0].icon + "@2x.png")
+      dailyWeatherEl.appendChild(weatherIcon);
+
+      // display temp
+      var dailyTempEl = document.createElement("span");
+      dailyTempEl.classList = "text-center card-item";
+      dailyTempEl.textContent = "Temp: " + dailyWeather.main.temp + " °F"
+      dailyWeatherEl.appendChild(dailyTempEl);
+
+      // display humidity
+      var dailyHumidityEl = document.createElement("span");
+      dailyHumidityEl.classList = "text-center card-item";
+      dailyHumidityEl.textContent = "Humidity: " + dailyWeather.main.humidity + "%";
+      dailyWeatherEl.appendChild(dailyHumidityEl);
+
+      // display wind
+      var dailyWindEl = document.createElement("span");
+      dailyWindEl.classList = "text-center card-item";
+      dailyHumidityEl.textContent = "Wind: " + dailyWeather.wind.speed + " MPH";
+      dailyWeatherEl.appendChild(dailyWindEl);
+    }
+};
+
+// build the cards for Past Searches
 var pastSearches = function(pastCity) {
   pastCity = pastCity.replace("+", " ");
-  console.log(pastCity);
-
+  
   var pastCityEl = document.createElement("button")
   pastCityEl.textContent = pastCity;
   pastCityEl.classList = "d-flex w-100 border text-center btn p-2";
   pastCityEl.setAttribute("past-city", pastCity);
   pastCityEl.setAttribute("type", "submit");
-
+  
   pastCityBtnEl.prepend(pastCityEl)
 }
 
+// this should be refactored for DRY code, but I would have to build two more functions up top
+// fetches queryURL to display weather for the past searched city cards
 var pastSearchesHandler = function(event) {
   var searchedCity = event.target.getAttribute("past-city");
   if (searchedCity) {
     city = searchedCity.replace(" ", "+");
-    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey +"&units=imperial";
+    queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey +"&units=imperial&cnt=5";
     fetch(queryURL)
-      .then(function(response) {
-    response.json().then(function(data) {
-      displayCurrentWeather(data, city);
+    .then(function(response) {
+      response.json().then(function(data) {
+        displayCurrentWeather(data, city);
+      });
     });
-  });
   }
 }
-
-
-
-// build cards dynamically to show the 5-day forecast
-
 
 // get the search info from the form on button "submit"
 formEl.addEventListener("submit", searchFormHandler);
